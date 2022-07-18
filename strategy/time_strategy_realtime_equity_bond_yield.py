426a72669696469bbe1161a931f97750f2ceaa4e
AdhocSun <1029791755@qq.com>
2022-07-18 23:56:42 +0800

    计算当前实时股债收益比的与历史数据相比的排位

--- strategy/time_strategy_realtime_equity_bond_yield.py
+++ strategy/time_strategy_realtime_equity_bond_yield.py
@@ -4,6 +4,7 @@
 
 import sys
 import time
+import decimal
 
 sys.path.append("..")
 import log.custom_logger as custom_logger
@@ -49,15 +50,41 @@ class TimeStrategyRealtimeEquityBondYield:
         # 获取实时十年期国债收益率
         realtime_ten_year_treasury_yield = float(get_target_real_time_indicator_from_interfaces.GetTargetRealTimeIndicatorFromInterfaces().get_real_time_treasury_yield("globalbd_gcny10"))
         # 计算实时股债收益比
-        realtime_equity_bond_yield = last_trading_day_CSI_300_yield_rate*(100-realtime_CSI_300_change)/100/realtime_ten_year_treasury_yield
+        realtime_equity_bond_yield = last_trading_day_CSI_300_yield_rate*(100-realtime_CSI_300_change)/realtime_ten_year_treasury_yield
         return realtime_equity_bond_yield
+
+
+    def calculate_current_realtime_equity_bond_yield_rank(self):
+        # 计算当前实时股债收益比的与历史数据相比的排位
+        # return：当前实时股债收益比和历史排位百分比  (2.8771481412772033, 83.88)
+        selecting_sql = "select ratio from stock_bond_ratio_di " \
+                        "where index_code = '000300' order by ratio desc"
+        # 查询
+        all_historical_ratio_list = db_operator.DBOperator().select_all("aggregated_data", selecting_sql)
+        # 实时股债收益比
+        realtime_equity_bond_yield_decimal = decimal.Decimal(self.calculate_realtime_equity_bond_yield())
+        # 历史上总共有多少数据量
+        all_historical_ratio_count = len(all_historical_ratio_list)
+
+        # 如果是极端值，大于历史上的最大值
+        if(realtime_equity_bond_yield_decimal>=all_historical_ratio_list[0]["ratio"]):
+            return (float(realtime_equity_bond_yield_decimal),100)
+        # 如果是极端值，小于历史上的最小值
+        elif(realtime_equity_bond_yield_decimal<all_historical_ratio_list[all_historical_ratio_count-1]["ratio"]):
+            return (float(realtime_equity_bond_yield_decimal), 0)
+        # 处于0%-100%之间
+        else:
+            for ratio_index in range(all_historical_ratio_count):
+                if(realtime_equity_bond_yield_decimal>all_historical_ratio_list[ratio_index]["ratio"]):
+                    return (float(realtime_equity_bond_yield_decimal), round(((1-ratio_index/all_historical_ratio_count)*100),2))
 
 if __name__ == '__main__':
     time_start = time.time()
     go = TimeStrategyRealtimeEquityBondYield()
     #result = go.get_realtime_CSI_300_change()
     #result = go.get_last_trading_day_CSI_300_yield_rate()
-    result = go.calculate_realtime_equity_bond_yield()
+    #result = go.calculate_realtime_equity_bond_yield()
+    result =go.calculate_current_realtime_equity_bond_yield_rank()
     print(result)
     time_end = time.time()
     print('Time Cost: ' + str(time_end - time_start))

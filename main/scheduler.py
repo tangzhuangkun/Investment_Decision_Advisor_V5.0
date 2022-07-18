dbc2368f30ebaf39f89ddf3151f4f6c000f23f09
AdhocSun <1029791755@qq.com>
2022-07-19 00:15:35 +0800

    每天创建一个日级日志文件

--- main/scheduler.py
+++ main/scheduler.py
@@ -34,9 +34,9 @@ class Scheduler:
 		try:
 			# 每天创建一个日级日志文件
 			scheduler.add_job(func=custom_logger.CustomLogger().daily_log_creator,
-							  trigger='cron',
-							  hour=1,minute=1,day_of_week='mon,tue,wed,thu,fri',
+							  trigger='cron',hour=0, minute=1, day_of_week='mon,tue,wed,thu,fri',
 							  id='createDailyLogFile')
+
 		except Exception as e:
 			# 抛错
 			custom_logger.CustomLogger().log_writter(e, 'error')

8e89b71a946a53ef01eedade6cbc23a402ecc35c
AdhocSun <1029791755@qq.com>
2022-08-03 22:45:42 +0800

    计算每一天的股债收益比,添加更多数据信息

--- data_miner/sql_query/cal_stock_bond_ratio.sql
+++ data_miner/sql_query/cal_stock_bond_ratio.sql
@@ -1,9 +1,13 @@
 /* 计算每一天的股债收益比 */
-insert into aggregated_data.stock_bond_ratio_di (index_code, index_name, trading_date, pe, stock_yield_rate,
-                                                 10y_bond_rate, ratio)
+insert into aggregated_data.stock_bond_ratio_di (index_code, index_name, trading_date, index_tv, index_ta, index_cp,
+                                                 index_cpc, pe, stock_yield_rate, 10y_bond_rate, ratio)
 select index_code,
        index_name,
        trading_date,
+       index_tv,
+       index_ta,
+       index_cp,
+       index_cpc,
        pe,
        /*  使用cast代替round，解决精度问题 */
        cast(1/pe as decimal(9,6))             as stock_yield_rate,
@@ -11,7 +15,7 @@ select index_code,
        cast(cast(1/pe as decimal(9,6)) / cast(10y/100 as decimal(9,6)) as decimal(20,17)) as ratio
 from (
     /*  使用沪深300市值加权的市盈率作为参考 */
-      (select index_code, index_name, pe_ttm_mcw as pe, trading_date
+      (select index_code, index_name, pe_ttm_mcw as pe, trading_date, tv as index_tv, ta as index_ta, cp as index_cp, cpc as index_cpc
       from index_estimation_from_lxr_di
       where index_code = '000300'
       order by trading_date desc) a

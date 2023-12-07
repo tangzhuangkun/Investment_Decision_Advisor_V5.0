032ccb1de5e5b93fffc8ccc3611da80f35033f23
AdhocSun <1029791755@qq.com>
2023-12-07 23:41:07 +0800

    从中证指数官网接口收集过去几年表现优异的指数，单线程，多线程，调整收益率要求，完成

--- data_collector/collect_excellent_index_from_cs_index_single_thread.py
+++ data_collector/collect_excellent_index_from_cs_index_single_thread.py
@@ -28,7 +28,7 @@ class CollectExcellentIndexFromCSIndexSingleThread:
 
         # 衡量标准
         # 3年年化收益率
-        self.three_year_yield_rate_standard = 8
+        self.three_year_yield_rate_standard = 10
         # 5年年化收益率
         self.five_year_yield_rate_standard = 10
         # 最大线程数
